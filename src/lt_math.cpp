5489647e3c28d03d464e6b39cd9cf921664cdf22
Leonardo <leo_lh324@hotmail.com>
2018-01-16 13:33:50 -0200

    Removed unnecessary comment.

--- src/lt_math.cpp
+++ src/lt_math.cpp
@@ -2,11 +2,6 @@
 #include <math.h>
 #include <stdio.h>
 
-/////////////////////////////////////////////////////////
-//
-// Vector implementation
-//
-
 Mat4f::Mat4f(f32 m00, f32 m01, f32 m02, f32 m03,
 			 f32 m10, f32 m11, f32 m12, f32 m13,
 			 f32 m20, f32 m21, f32 m22, f32 m23,
